3754187c3944620937710decb415de1bf959e792
Shikomory <NeXoZplay@yandex.ru>
2025-04-14 00:01:08 +0300

    добавлены уточнения для исключений

--- Task6/src/main/java/com/sbertech/Multiple.java
+++ Task6/src/main/java/com/sbertech/Multiple.java
@@ -3,8 +3,14 @@ package com.sbertech;
 public class Multiple {
 
     public int multiply(int a, int b) throws EvenException{
-        if (a % 2 != 0 || b % 2 != 0) {
-            throw new EvenException("нечетное число");
+        if (a % 2 != 0 && b % 2 != 0) {
+            throw new EvenException("Оба числа нечетные: " + a + ", " + b);
+        }
+        if (a % 2 != 0) {
+            throw new EvenException("первое число нечетное: " + a);
+        }
+        if (b % 2 != 0) {
+            throw new EvenException("второе число нечетное: " + b);
         }
         return a * b;
     }
