8cedbd33d9b6436d9ce0fb3f69be77c4e4309fac
Shikomory <NeXoZplay@yandex.ru>
2025-04-17 19:50:14 +0300

    небольшие исправления

--- Task9/src/main/java/com/sbertech/Db.java
+++ Task9/src/main/java/com/sbertech/Db.java
@@ -14,38 +14,6 @@ public class Db {
         initTable();
     }
 
-    void add(String task, String status) throws SQLException {
-        String sql = "INSERT INTO users (Name, Status) VALUES (?, ?)";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setString(1, task);
-            statement.setString(2, status);
-            statement.executeUpdate();
-        }
-    }
-
-    void show() throws SQLException{
-        String sql = "SELECT * FROM users";
-        try (Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(sql)) {
-            System.out.println("Список задач:");
-            while (resultSet.next()) {
-                int uid = resultSet.getInt("UID");
-                String name = resultSet.getString("Name");
-                String status = resultSet.getString("Status");
-                System.out.printf("UID: %d, Name: %s, Status: %s%n", uid, name, status);
-            }
-        }
-    }
-
-    void done(String name) throws SQLException{
-      String sql = "UPDATE users SET Status = 'Done' WHERE Name = '" + name + "'";
-//        String sql = "UPDATE users SET Status = ? WHERE Name = ?;";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-//            statement.setString(1, "Done");
-            statement.executeUpdate();
-        }
-    }
-
     private void createTable(Connection connection) throws SQLException {
         String sql = """
                 CREATE TABLE IF NOT EXISTS tasks (
@@ -65,7 +33,9 @@ public class Db {
         try  {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Соединение с H2 установлено!");
+            if (connection != null) {
             createTable(connection);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
