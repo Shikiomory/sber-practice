ce9d74a9d829cee8c958b8d94c6406763151c13f
Shikomory <NeXoZplay@yandex.ru>
2025-04-17 19:47:54 +0300

    небольшие исправления

--- Task9/src/main/java/com/sbertech/App.java
+++ Task9/src/main/java/com/sbertech/App.java
@@ -1,6 +1,5 @@
 package com.sbertech;
 import java.sql.*;
-import java.util.Scanner;
 
 public class App {
     private static final String URL = "jdbc:h2:mem:testdb";
@@ -8,8 +7,6 @@ public class App {
     private static final String PASSWORD = "";
 
     public static void main(String[] args) throws SQLException {
-        Scanner scanner = new Scanner(System.in);
-
         Db db = new Db(URL, USER, PASSWORD);
         Console console = new Console(db);
         console.exec();
